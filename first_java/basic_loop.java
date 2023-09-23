ceeb43491dc7bf0ac17aef2f189b6f313e113a3e
Isha Patel <96524214+hohoh12@users.noreply.github.com>
2023-09-23 22:26:22 +0530

    Add files via upload

---
+++ first_java/basic_loop.java
@@ -0,0 +1,15 @@
+package com.first_java;
+
+public class basic_loop {
+    public static void main(String[] args) {
+//        int a = 10;
+//        if(a==10){
+//            System.out.println("Hello");
+
+        int count = 1;
+        while(count !=5){
+            System.out.println(count);
+            count++;
+        }
+    }
+}
